9127dfe8c233a0e4cb3175320d6ce7d1f356bf37
sullihuk <nyxa@nextmail.ru>
2022-06-15 06:40:00 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-    root 'application#hello'
-  end
+  get 'static_pages/about'
+  get 'static_pages/contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
